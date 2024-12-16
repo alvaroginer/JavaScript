1b7cb989e6fd9760320fabcc9ddf41b80c58aeb9
alvaroginer <aginer079j@gmail.com>
2024-12-16 23:20:31 +0100

    más katas

--- Primera prueba/ejercicios KATAS/kastas2.js
+++ Primera prueba/ejercicios KATAS/kastas2.js
@@ -74,3 +74,38 @@ function titleCase(title, minorWords) {
 }
 
 console.log(titleCase("the quick brown fox"));
+
+// hacer un sort de A
+// hacer un forEach de cada letter de A a una nueva variable
+// separar el array con un espacio mediante un condicional en el que ponga que if (letter !== array[index-1]){newArr.push(' ')}
+// coger el nuevo array y hacer un join(' ')
+// hacer un find de newArr y poner un condicional de si numbers.length % === 0 return numbers[0]
+
+function findOdd(A) {
+  const sortedA = A.sort((a, b) => a - b);
+  const newArr = [];
+  sortedA.forEach(function (letter, index) {
+    if (letter !== A[(index - 1) % A.length]) {
+      newArr.push(" ");
+    }
+    newArr.push(letter);
+  });
+
+  const arrWithOut0 = newArr.slice(1);
+
+  const numToString = arrWithOut0.map(function (numbers) {
+    numbers = numbers.toString(numbers);
+    return numbers;
+  });
+
+  console.log(numToString);
+
+  const arrOdd = arrWithOut0.find(function (numbers) {
+    if (numbers.length % 2 !== 0) {
+      return numbers[0];
+    }
+  });
+  return arrOdd;
+}
+
+console.log(findOdd([20, 1, -1, 2, -2, 3, 3, 5, 5, 1, 2, 4, 20, 4, -1, -2, 5]));
