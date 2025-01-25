f2c9224ae6793759fd406e1ae973fb98068fc2be
alvaroginer <aginer079j@gmail.com>
2025-01-25 12:44:22 +0100

    más ejercicios de javascript en web

--- Ejercicios/Ejercicio JavaScript Web/index.js
+++ Ejercicios/Ejercicio JavaScript Web/index.js
@@ -38,7 +38,43 @@ function renderDogArray() {
   });
 }
 
-// Arreglar contador
+// Función que añade un número N de perros
+const addNDogs = async (num) => {
+  for (let i = 0; i < num; i++) {
+    const dogImage = await getRandomDogImage();
+    perricosArray.push(dogImage);
+
+    const addHtml = `<div class="container">
+        <div class="img-container">
+          <img src="${dogImage}" alt="" />
+        </div>
+        <div class="emoji-container">
+          <p>❤️ <span class="like-counter">0</span< </p>
+          <p>🤬:0</p>
+        </div>
+        <div class="emoji-container">
+          <button class="like-button">Me gusta</button>
+          <button class="not-like-button">No me gusta</button>
+        </div>
+      </div>`;
+
+    const listDogs = document.querySelector(".dog-container");
+    listDogs.innerHTML += addHtml;
+
+    const likeCountNodes = document.querySelectorAll(".like-counter");
+
+    document.querySelectorAll(".like-button").forEach((element, index) => {
+      element.addEventListener("click", function () {
+        console.log("este código funciona");
+
+        likeCountNodes[index].textContent =
+          Number(likeCountNodes[index].textContent) + 1;
+      });
+    });
+  }
+};
+
+// Contador total de perros
 let totalDogsNum = 1;
 function renderDogCounter(num) {
   const totalDogsCounter = document.querySelector("#totalDogsCounter");
@@ -51,17 +87,6 @@ function renderDogCounter(num) {
   totalDogsCounter.textContent = finalCount.join(": ");
 }
 
-// Función que añade un número N de perros
-const addNDogs = async (num) => {
-  for (let i = 0; i < num; i++) {
-    const dogImage = await getRandomDogImage();
-    perricosArray.push(dogImage);
-    renderDogArray();
-  }
-};
-
-renderDogArray();
-
 document.querySelector("#add-1-dog").addEventListener("click", function () {
   addNDogs(1);
   renderDogCounter(1);
@@ -71,3 +96,5 @@ document.querySelector("#add-5-dog").addEventListener("click", function () {
   addNDogs(5);
   renderDogCounter(5);
 });
+
+renderDogArray();
