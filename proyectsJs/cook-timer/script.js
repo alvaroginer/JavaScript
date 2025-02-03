9bf4e2a1aa7aa0d26fe1c12a9435a82d05e797e8
alvaroginer <aginer079j@gmail.com>
2025-02-03 17:51:44 +0100

    falta crear función

--- proyectsJs/cook-timer/script.js
+++ proyectsJs/cook-timer/script.js
@@ -39,7 +39,7 @@ const createCounter = () => {
               <button class="timer-counter-container--button timer-counter-container--button-min">Minutes</button>
               <button class="timer-counter-container--button timer-counter-container--button-sec">Seconds</button>
             </div>
-            <p class="font-size__60 margin__15 color__yellow">00:00</p>
+            <p class="font-size__60 margin__15 color__yellow timer-counter-container--text">00:00</p>
             <div class="display__flex">
               <button class="timer-counter-container--button">+</button>
               <button class="counter-container--button color__white">
@@ -49,6 +49,15 @@ const createCounter = () => {
             </div>`;
   const timeContainer = document.querySelector(".timer--container");
   timeContainer.appendChild(timerNode);
+};
+
+const modifyTime = () => {
+  // const minuteNode =
+  // const secondNode =
+  const time = document.querySelector(
+    ".timer-counter-container--text"
+  ).textContent;
+  let [minutes, seconds] = time.split(":");
 };
 
 document
