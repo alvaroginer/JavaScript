9df74d99b9925f547b56b87d0d28386d4f928efc
alvaroginer <aginer079j@gmail.com>
2025-02-25 15:14:04 +0100

    implemented general score rate

--- proyectsJs/calling-panel/index.js
+++ proyectsJs/calling-panel/index.js
@@ -12,7 +12,54 @@ const person = {
       callClosure: { score: 0, weight: 0.2 },
       callDuration: { score: 0, weight: 0.1 },
       technicalQuality: { score: 0, weight: 0.15 },
-      rating: 5,
     },
   ],
+  callsRating: 0,
+  emails: {
+    sent: 4,
+    open: 2,
+    clicked: 1,
+    openRate: 50,
+    block: 0,
+    rebound: 0,
+    rating: 0,
+  },
+
+  overallRating: 0,
+};
+
+const calculateGeneralRating = (person) => {
+  // Calculamos el Mail Rating
+  const emailValue =
+    person.emails.sent * 0.1 +
+    person.emails.open * 0.2 +
+    person.emails.clicked * 0.3 +
+    person.emails.openRate * 0.2 +
+    person.emails.block * -0.1 +
+    person.emails.rebound * -0.1;
+  const emailRating = (emailValue / 2) * 5;
+  person.emails.rating = emailRating;
+
+  //Calculamos el Call Rating
+  const callsRating = [];
+  person[calls].forEach((call) => {
+    const callRate =
+      person.calls.customerInterest * 0.2 +
+      person.calls.objectionsRaised * 0.15 +
+      person.calls.conversionPotential * 0.2 +
+      person.calls.callClosure * 0.2 +
+      person.calls.callDuration * 0.1 +
+      person.calls.technicalQuality * 0.15;
+    callsRating.push(callRate);
+  });
+
+  const sumAllRates = callsRating.reduce(
+    (accumulator, currentValue) => accumulator + currentValue,
+    0
+  );
+  const totalCallsRating = sumAllRates / callsRating.length;
+  person.callsRating = totalCallsRating;
+
+  //Final rating
+  person.overallRating = emailRating + totalCallsRating;
 };
