26d8ba13c1508d4a611032c37f4e59a3bd77ad5b
alvaroginer <aginer079j@gmail.com>
2025-02-25 08:37:08 +0100

    working on new proyect

---
+++ proyectsJs/calling-panel/index.js
@@ -0,0 +1,18 @@
+//Tipo de objeto que hay que pasar
+const person = {
+  id: 12345,
+  name: "John Doe",
+  number: "+1234567890",
+  calls: [
+    {
+      number: 1,
+      customerInterest: { score: 0, weight: 0.2 },
+      objectionsRaised: { score: 0, weight: 0.15 },
+      conversionPotential: { score: 0, weight: 0.2 },
+      callClosure: { score: 0, weight: 0.2 },
+      callDuration: { score: 0, weight: 0.1 },
+      technicalQuality: { score: 0, weight: 0.15 },
+      rating: 5,
+    },
+  ],
+};
