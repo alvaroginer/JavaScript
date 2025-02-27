b4f3443e865e096a232235b28083e5579be00eca
alvaroginer <aginer079j@gmail.com>
2025-02-27 22:01:45 +0100

    working on the basic functionalities

--- proyectsJs/calling-panel/index.js
+++ proyectsJs/calling-panel/index.js
@@ -39,6 +39,97 @@ const person1 = {
   overallRating: 0,
 };
 
+//Función para extraer información del archivo json
+async function extractDataJson() {
+  try {
+    const answer = await fetch("./data.json"); // Ruta del archivo JSON
+    if (!answer.ok) {
+      throw new Error("Error al cargar el JSON");
+    }
+    const data = await answer.json();
+    return data.persons;
+    console.log(data); // Aquí puedes usar los datos como un objeto
+  } catch (error) {
+    console.error("Hubo un problema con la carga del JSON:", error);
+  }
+}
+
+const uploadData = async () => {
+  const usersData = await extractDataJson();
+  localStorage.setItem("users", JSON.stringify(usersData));
+};
+
+uploadData();
+
+//Función para crear el div de la llamada
+// Hacer prependChild y ordenar las llamadas de última a anterior
+
+//renderizamos la información y creamos el html con los user-cards
+const usersContainer = document.querySelector(".users-grid");
+const renderData = () => {
+  const usersLocalStorage = JSON.parse(localStorage.getItem("users"));
+  usersLocalStorage.forEach((user) => {
+    const userCard = document.createElement("div");
+    userCard.className = "event-card user-card grid-cell margin--bt__24";
+    userCard.innerHTML = `<div class="event-card--text">
+          <div class="display--flex">
+            <div>
+              <div class="rating-container">${user.overallRating}</div>
+              <p class="margin-none text-align-center font-size__12">
+                ID: ${user.id}
+              </p>
+            </div>
+            <div
+              class="event-card--text__p display--flex flex-direction-column margin--left__24"
+            >
+              <h3 class="event-card--text__title">${user.name}</h3>
+              <div class="display--flex">
+                <p class="margin-none color-orange">Estado:</p>
+                <select name="userState" id="" class="margin--left__6">
+                  <option class="" value="text">Interesado</option>
+                  <option value="text">No interesado</option>
+                  <option value="text">Conversión</option>
+                </select>
+              </div>
+            </div>
+          </div>
+          <hr />
+          <div
+            class="event-card--button-container display--flex space--between"
+          >
+            <button class="button-container--button calls-button-section">
+              <img src="./imgs/phone.svg" alt="" />
+            </button>
+            <button class="button-container--button mails-button-section">
+              <img src="./imgs/email.svg" alt="" />
+            </button>
+            <button class="button-container--button notes-button-section">
+              <img src="./imgs/account-box-edit-outline.svg" alt="" />
+            </button>
+          </div><div class="event-card--sub-section"></div>`;
+    usersContainer.appendChild(userCard);
+
+    userCard
+      .querySelector(".calls-button-section")
+      .addEventListener("click", function () {
+        console.log("botón llamada funciona");
+      });
+
+    userCard
+      .querySelector(".mails-button-section")
+      .addEventListener("click", function () {
+        console.log("botón mail funciona");
+      });
+
+    userCard
+      .querySelector(".notes-button-section")
+      .addEventListener("click", function () {
+        console.log("botón notas funciona");
+      });
+  });
+};
+renderData();
+
 const calculateGeneralRating = (person) => {
   // Calculamos el Mail Rating
   const emailValue =
@@ -77,8 +168,9 @@ const calculateGeneralRating = (person) => {
   return person;
 };
 
-console.log(calculateGeneralRating(person1));
+//console.log(calculateGeneralRating(person1));
 
+// More functionalities
 /* Sort by:
  - Call Rating
  - Email Rating
